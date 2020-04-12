8b9b4734dfda70532c8665dcdaf42df8dc4f67ad
Nikita Babko <niki.babko@gmail.com>
2020-04-12 19:58:45 +0700

    text changed

--- src/components/SectionIntro/IntroModal.js
+++ src/components/SectionIntro/IntroModal.js
@@ -5,13 +5,12 @@ class IntroModal extends Component {
         closeModal() {
             const closeModal = document.querySelector(".introModal");
             closeModal.style.display = "none";
-        }
+        },
     };
     render() {
         return (
             <div className="introModal">
                 <div>Welcome</div>
-                <div>My nickname - White Rabbit</div>
                 <div className="close-modal" onClick={this.state.closeModal}>
                     Close
                 </div>
