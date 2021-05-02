9ba073f0938485f6ecf5e2ec191d4e848aab1cfe
Nikita Babko <niki-babko@yandex.ru>
2021-05-02 20:00:44 +0700

    Linting.

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,37 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 // Components
-import Header from "./Header/Header";
-import SectionIntro from "./SectionIntro/SectionIntro";
-import SectionFeatures from "./SectionFeatures";
-import SectionWorks from "./SectionWorks";
-import SectionOurTeam from "./SectionOurTeam";
-import SectionTestimonials from "./SectionTestimonials/SectionTestimonials";
-import SectionDownload from "./SectionDownload";
-import Footer from "./Footer";
+import Header from './Header/Header';
+import SectionIntro from './SectionIntro/SectionIntro';
+import SectionFeatures from './SectionFeatures';
+import SectionWorks from './SectionWorks';
+import SectionOurTeam from './SectionOurTeam';
+import SectionTestimonials from './SectionTestimonials/SectionTestimonials';
+import SectionDownload from './SectionDownload';
+import Footer from './Footer';
 
 // Library
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 // Styles
-import "../scss/index.scss";
+import '../scss/index.scss';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <SectionIntro />
-                <SectionFeatures />
-                <SectionWorks />
-                <SectionOurTeam />
-                <SectionTestimonials />
-                <SectionDownload />
-                <Footer />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <SectionIntro />
+        <SectionFeatures />
+        <SectionWorks />
+        <SectionOurTeam />
+        <SectionTestimonials />
+        <SectionDownload />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
